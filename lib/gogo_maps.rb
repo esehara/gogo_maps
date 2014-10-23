36129ad49529e58d62f6608c8da2f97c7a4b792f
gogotanaka <mail@tanakakazuki.com>
2014-10-23 13:19:25 -0700

    Finish implementing anyway

--- lib/gogo_maps.rb
+++ lib/gogo_maps.rb
@@ -1,12 +1,41 @@
 require "gogo_maps/version"
+require 'faraday'
+require 'json'
 
 module GogoMaps
-  # @param Hash opts - to support below Ruby1.9x.
-  def self.parameters(opts={ address: nil, latlng: nil, language: :ja, sensor: false })
+   # @param Hash opts - to support below Ruby1.9x.
+  def self.get(opts={})
     fail 'Should provide either address or latlng' unless opts[:address] || opts[:latlng]
-    opts.inject('?') do |param, (k, v)|
-      next unless v
-      param += "#{k}=#{v}&"
+
+    GoogleMapClient.call(
+      { language: :ja, sensor: false }.merge(opts),
+      opts[:address] ? :to_latlng : :to_address
+    )
+  end
+
+  class GoogleMapClient
+    ENDPOINT = 'http://maps.googleapis.com'
+    @@_conn ||= Faraday.new(url: ENDPOINT) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    def self.call(params, sym)
+      response = @@_conn.get do |req|
+        req.url '/maps/api/geocode/json'
+        req.params = params
+      end
+
+      unless location = JSON.parse(response.body)['results'][0]
+        fail 'Something wrong with Google API or your params'
+      end
+
+      case sym
+        when :to_latlng  then location['geometry']['location']
+        when :to_address then location['formatted_address']
+      end
     end
   end
+
 end
--- spec/gogo_maps_spec.rb
+++ spec/gogo_maps_spec.rb
@@ -5,13 +5,40 @@ describe GogoMaps do
     expect(GogoMaps::VERSION).not_to be nil
   end
 
-  describe '#parameters' do
+  describe '.get' do
     it 'should raise error' do
-      expect { GogoMaps.parameters }.to raise_error(RuntimeError)
+      expect { GogoMaps.get }.to raise_error(RuntimeError)
     end
 
-    it 'should return proper parameters' do
-      expect(GogoMaps.parameters(address: '長野県高山村')).to eq "?address=長野県高山村&"
+    context 'to_latlng' do
+      it 'should return proper parameters' do
+        expect(
+          GogoMaps.get(address: '長野県上高井郡高山村')
+        ).to eq(
+          {"lat"=>36.6797676, "lng"=>138.3632554}
+        )
+        expect(
+          GogoMaps.get(address: '神奈川県横浜市港北区日吉')
+        ).to eq(
+          {"lat"=>35.5565107, "lng"=>139.6460026}
+        )
+        expect(
+          GogoMaps.get(address: '902 Rockefeller Dr, Sunnyvale, CA', language: :en)
+        ).to eq(
+          {"lat"=>37.358126, "lng"=>-122.050636}
+        )
+      end
     end
+
+    context 'to_address' do
+      it 'should return proper parameters' do
+        expect(
+          GogoMaps.get(latlng: '35.6506135,139.7539103')
+        ).to eq(
+          '日本, 東京都港区芝１丁目１１−１４'
+        )
+      end
+    end
+
   end
 end
