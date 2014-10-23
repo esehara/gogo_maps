2ea3ca7656f69d982aa3a5662e8e280d1a25ed28
gogotanaka <mail@tanakakazuki.com>
2014-10-23 12:27:55 -0700

    Implement #parameters

--- lib/gogo_maps.rb
+++ lib/gogo_maps.rb
@@ -1,5 +1,12 @@
 require "gogo_maps/version"
 
 module GogoMaps
-
+  # @param Hash opts - to support below Ruby1.9x.
+  def self.parameters(opts={ address: nil, latlng: nil, language: :ja, sensor: false })
+    fail 'Should provide either address or latlng' unless opts[:address] || opts[:latlng]
+    opts.inject('?') do |param, (k, v)|
+      next unless v
+      param += "#{k}=#{v}&"
+    end
+  end
 end
